d39b2b8a6d50576abe3523d429ac33d98a767752
Konrad <saitan@vp.pl>
2025-05-05 14:28:28 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -28,6 +28,15 @@ resource "azurerm_subnet" "db_subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
+
+  delegation {
+      name = "delegation"
+  
+      service_delegation {
+        name    = "Microsoft.DBforMySQL/flexibleServers"
+        #actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+      }
+    }
 }
 
 // MySQL Database

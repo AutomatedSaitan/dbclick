23cc2d37129eb950cbef3132938bb71571c379a1
Konrad Dudek <kondud91@gmail.com>
2025-05-05 21:01:52 +0200

    correct plan id in app def

--- main.tf
+++ main.tf
@@ -86,7 +86,7 @@ resource "azurerm_linux_web_app" "app" {
   name                = "dbclick-app"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  service_plan_id     = azurerm_app_service_plan.app_plan.id
+  service_plan_id     = azurerm_service_plan.app_plan.id
 
 
   site_config {

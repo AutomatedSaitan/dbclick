e4b2083796a2114262ea74b754b6512f04433f85
Konrad Dudek <kondud91@gmail.com>
2025-05-05 22:22:38 +0200

    switching from private endpoints to subnet pearing

--- main.tf
+++ main.tf
@@ -21,6 +21,13 @@ resource "azurerm_subnet" "app_subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
+  delegation {
+    name = "app-delegation"
+    service_delegation {
+      name    = "Microsoft.Web/serverFarms"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
+  }
 }
 
 resource "azurerm_subnet" "db_subnet" {
@@ -28,59 +35,25 @@ resource "azurerm_subnet" "db_subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
-
   delegation {
-      name = "delegation"
-  
-      service_delegation {
-        name    = "Microsoft.DBforMySQL/flexibleServers"
-        actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
-      }
+    name = "db-delegation"
+    service_delegation {
+      name    = "Microsoft.DBforMySQL/flexibleServers"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
     }
+  }
 }
 
 // MySQL Database
 resource "azurerm_mysql_flexible_server" "db" {
-  name                = "dbclick-mysql"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  administrator_login = var.db_user
+  name                   = "dbclick-mysql"
+  resource_group_name    = azurerm_resource_group.rg.name
+  location               = azurerm_resource_group.rg.location
+  administrator_login    = var.db_user
   administrator_password = var.db_password
-  sku_name            = "B_Standard_B1ms"
-  delegated_subnet_id = azurerm_subnet.db_subnet.id
-}
-
-// Private DNS Zone
-resource "azurerm_private_dns_zone" "mysql_dns" {
-  name                = "privatelink.mysql.database.azure.com"
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-resource "azurerm_private_dns_zone_virtual_network_link" "mysql_dns_link" {
-  name                  = "mysql-dns-link"
-  resource_group_name   = azurerm_resource_group.rg.name
-  private_dns_zone_name = azurerm_private_dns_zone.mysql_dns.name
-  virtual_network_id    = azurerm_virtual_network.vnet.id
-}
-
-// Private Endpoint
-resource "azurerm_private_endpoint" "mysql_pe" {
-  name                = "mysql-private-endpoint"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  subnet_id           = azurerm_subnet.app_subnet.id
-
-  private_service_connection {
-    name                           = "mysql-private-connection"
-    private_connection_resource_id = azurerm_mysql_flexible_server.db.id
-    subresource_names             = ["mysqlServer"]
-    is_manual_connection          = false
-  }
-
-  private_dns_zone_group {
-    name                 = "mysql-dns-zone-group"
-    private_dns_zone_ids = [azurerm_private_dns_zone.mysql_dns.id]
-  }
+  sku_name              = "B_Standard_B1ms"
+  delegated_subnet_id    = azurerm_subnet.db_subnet.id
+  private_dns_zone_id    = "System"
 }
 
 // App Service
@@ -99,7 +72,7 @@ resource "azurerm_linux_web_app" "app" {
   service_plan_id = azurerm_service_plan.app_plan.id
   
   app_settings = {
-    DB_HOST     = azurerm_private_endpoint.mysql_pe.private_service_connection[0].private_ip_address
+    DB_HOST     = azurerm_mysql_flexible_server.db.fqdn
     DB_USER     = var.db_user
     DB_PASSWORD = var.db_password
     DB_NAME     = "dbclick"

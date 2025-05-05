ca09f42893773ac347e317d0bc9c31f80849d065
Konrad <saitan@vp.pl>
2025-05-05 16:44:48 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ resource "azurerm_app_service_plan" "app_plan" {
   }
 }
 
-resource ""azurerm_linux_web_app"" "app" {
+resource "azurerm_linux_web_app" "app" {
   name                = "dbclick-app"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name

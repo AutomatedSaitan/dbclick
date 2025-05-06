1a07e1daf72643cf5ce45a135824186f1cb8aee8
Konrad Dudek <kondud91@gmail.com>
2025-05-06 12:05:54 +0200

    OIDC change

--- backend.tf
+++ backend.tf
@@ -4,9 +4,6 @@ terraform {
     storage_account_name = "tfstatedbclick"
     container_name       = "tfstate"
     key                 = "terraform.tfstate"
-    use_azuread_auth    = true
-    use_oidc            = true
-    subscription_id     = "250d1287-152a-48e8-8b1c-2e7a9a8b3256"
-    tenant_id           = "4b1d7455-1388-4836-b4b9-0095dd2f4c45"
+    use_cli            = true
   }
 }

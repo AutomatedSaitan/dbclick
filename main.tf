7d3750b739b413dd88c3edf4db95447cac202c5c
Konrad Dudek <kondud91@gmail.com>
2025-05-06 21:28:15 +0200

    Region change

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "azurerm_role_assignment" "user_access_admin" {
 
 resource "azurerm_resource_group" "rg" {
   name     = "rg-dbclick"
-  location = "Sweden Central"
+  location = "Norway East"
 }
 
 // VNet and Subnets

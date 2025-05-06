fa10517fc683b9c0ed6d0713b9454c27f6bdc64d
Konrad Dudek <kondud91@gmail.com>
2025-05-06 11:54:55 +0200

    implement backend for terraform

---
+++ backend.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "azurerm" {
+    storage_account_name = "tfstatedbclick"
+    container_name       = "tfstate"
+    key                 = "terraform.tfstate"
+    use_oidc            = true
+    subscription_id     = "250d1287-152a-48e8-8b1c-2e7a9a8b3256"
+    tenant_id           = "4b1d7455-1388-4836-b4b9-0095dd2f4c45"
+  }
+}

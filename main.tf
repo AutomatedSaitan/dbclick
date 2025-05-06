a5e2f533207c1b9a1a083b7c6322d7e963a80f30
Konrad Dudek <kondud91@gmail.com>
2025-05-06 17:13:45 +0200

    temp switch ACR name to set string

--- main.tf
+++ main.tf
@@ -195,6 +195,8 @@ resource "azurerm_linux_web_app" "app" {
   site_config {
     always_on = false
     vnet_route_all_enabled = true
+    container_registry_managed_identity_client_id = [data.azurerm_user_assigned_identity.app_identity.id]
+    container_registry_use_managed_identity       = true
     application_stack {
       docker_image_name   = "dbclick-app:latest"
       docker_registry_url = "https://${azurerm_container_registry.acr.login_server}"
